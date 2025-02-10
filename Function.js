505fc86b290f6fd0d2bbc3e9fb276672fcfe1b62
Muneeb Khalsai <muneeb.akhlaq05@gmail.com>
2025-02-10 18:37:19 +0500

    function

---
+++ Function.js
@@ -0,0 +1,72 @@
+// function Hello() {
+//  console.log('Hello Function');   
+// }
+// Hello();
+
+// function counting() {
+//     for(let i = 0; i<100; i++){
+//         console.log(i);
+//     }
+    
+// }
+// counting();
+
+// function printnumber(num) {
+//     console.log('Print Number',num);
+
+// }
+// printnumber(5);
+
+
+// function sum(a,b) {
+//     sum=a+b;
+//     console.log('The Sum Of A and B is',sum);
+    
+// }
+// sum(4,6)
+
+
+// function getsum(a,b,c) {
+//    let  sum = a+b+c;
+//     return sum;
+
+    
+// }
+// let ans = getsum(1,4,5);
+// console.log(ans);
+
+// function fulname(first,last){
+// //     let name = first + "" + last;
+// //     return name;
+
+
+// // }   
+// // let ans = fulname(Khalsai);
+// // console.log(ans); 
+ 
+// function cube(a,b) {
+//     let cube=a*b;
+//     return cube;
+    
+
+// }
+// let ans = cube(2,5);
+// console.log(ans);
+
+
+// function multiply(a,b) {
+//     let multiplication = a*b;
+//     return multiplication;
+// }
+// let multi = multiply(6,6);
+// console.log(multi);
+
+
+//Arrow Funciton
+
+// let sum =(a,b)=>{
+//     return a+b;
+// }
+// let ans = sum(3,5);
+// console.log(ans);
+
