2361068db7205fc5cd90789b54bfd45e215ff102
Muneeb Khalsai <muneeb.akhlaq05@gmail.com>
2025-02-12 07:54:51 +0500

    complete

---
+++ Hoisting.js
@@ -0,0 +1,93 @@
+// hello('Muneeb');
+
+// function hello(Yourname) {
+//     console.log(Yourname);
+    
+// }
+
+
+
+// hello('Khalsai');
+// let hello = function (Yourname){
+//     console.log(Yourname);
+// }
+
+
+
+
+// console.log(sum);
+// var sum = 10;
+
+// console.log(sum);
+// let sum = 10;
+
+
+// console.log(sum);
+// const sum = 10;
+
+// function Fathername() {
+//     console.log('Muneeb Khalsai');
+//     name();
+// }
+// Fathername();
+
+// function name() {
+//     console.log('Hello World');
+    
+// }
+
+
+// function Hello (name){
+//     console.log('Hello' , name);
+// }
+// Hello ('Muneeb');
+
+
+
+// Function In Arrays
+
+// let arr = [
+//     function(a,b){
+//         return a+b;
+//     },
+
+//     function(a,b){
+//         return a-b;
+//     },
+
+
+//     function(a,b){
+//         return a*b;
+//     },
+
+
+//     function(a,b){
+//         return a/b;
+//     }
+// ];
+
+// let first = arr[2];
+// let ans = first(6,8);
+// console.log(ans);
+
+
+
+// let obj = {
+//     fname : 'Muneeb',
+//     age: 25,
+//     weight: 55,
+//     height: 7,
+//     greet: ()=> {
+//         console.log('Hello World');
+//     } 
+
+// }
+
+// console.log(obj.weight);
+// obj.greet();
+
+
+// let greet = function() {
+//     console.log('Hello World');
+// }
+// greet();    
